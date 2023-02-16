00dc6726e8a53ad72cf57a9f6b8b328ec1a885a0
Supertigerr <saawalkhn@googlemail.com>
2023-02-16 14:34:35 +0000

    Hopefully now it exports correctly

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'tsup-preset-solid'
 
 export default defineConfig(
-  {
-    entry: 'src/index.tsx',
-    devEntry: true,
-  },
+  [
+    {
+      entry: 'src/index.tsx',
+      devEntry: true,
+    },
+    {
+      entry: 'src/emojis.ts',
+      devEntry: true,
+    },
+    {
+      entry: 'src/categories.ts',
+      devEntry: true,
+    },
+  ],
   {
     // Enable this to write export conditions to package.json
-    // writePackageJson: true,
+    writePackageJson: true,
     dropConsole: true,
     cjs: true,
   },
